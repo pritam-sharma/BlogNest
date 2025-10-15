5b4f403a48dce2d6fc64f9c67d03d9adddaf6c6a
pritam-sharma <pritamsharma531@gmail.com>
2025-10-15 19:34:23 +0530

    fix : add post issue

--- Frontend/src/components/AuthRoute/ProtectedRoute.jsx
+++ Frontend/src/components/AuthRoute/ProtectedRoute.jsx
@@ -3,7 +3,8 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const { userAuth } = useSelector((state) => state.users);
-  const isLoggedIn = userAuth?.userAuth?.token;
+  console.log("user auth in protected route : ", userAuth);
+  const isLoggedIn = userAuth?.userInfo?.token;
   if (!isLoggedIn) {
     return <Navigate to="/login" />;
   }
